cc43e6cd7fb6e1771b8f4aede59ce0f7e9f769cc
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-12 14:18:10 +0300

    add chart component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,5 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
-import {Sparklines,SparklinesLine} from 'react-sparklines'
 
 class WeatherList extends Component{
 
@@ -12,9 +11,7 @@ class WeatherList extends Component{
             <tr key={name}>
                 <td>{name}</td>
                 <td>
-                    <Sparklines height={120} width={180} data = {temps}>
-                        <SparklinesLine color="red"/>
-                    </Sparklines>
+                    
                 </td>
             </tr>
         )
